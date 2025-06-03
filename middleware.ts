f23a2360e09d3f352cb564b0a49ea790390c2fe9
Anirban Kapil <anirbankapil@gmail.com>
2025-06-03 10:57:59 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -24,16 +24,15 @@ export default clerkMiddleware((auth,req)=>{
       
       if(!userId){
         if(!isPublicRoute(req) && !isPublicApiRoute(req)){
-          
+        return NextResponse.redirect(new URL("/signin",req.url)) 
+        }
+        if(isApiRequest && !isPublicApiRoute(req)){
+        return NextResponse.redirect(new URL("/signin",req.url))
         }
       }
+      return NextResponse.next()
 })
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 }
