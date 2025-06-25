eb4251d314df29294d5086175f4f09a5774c3d7e
Anirban Kapil <anirbankapil@gmail.com>
2025-06-25 22:23:00 +0530

    update dependencies: upgrade @prisma/client, @types/node, prisma, tsx to latest versions

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -2,7 +2,7 @@ import { NextResponse , NextRequest } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient()   
 
 export async function GET (request : NextRequest) {
     try {
