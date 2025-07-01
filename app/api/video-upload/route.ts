9699bef0c12bb0bc949f4be384ab51821f5c0a1d
Anirban Kapil <anirbankapil@gmail.com>
2025-07-01 20:15:56 +0530

    bug fix

--- app/api/video-upload/route.ts
+++ app/api/video-upload/route.ts
@@ -1,8 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { NextRequest , NextResponse } from 'next/server';
 import { auth } from '@clerk/nextjs/server';
+import { PrismaClient } from '@/generated/prisma';
 
-
+const prisma = new PrismaClient()
 
 cloudinary.config({ 
         cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME, 
@@ -12,30 +13,36 @@ cloudinary.config({
 
 
 interface CloudinaryUploadResult {
-    public_id : string,
+    public_id : string;
+    bytes : number;
+    duration? : number;
     [key : string] : any
 }
 
 
 export async function POST (request : NextRequest) {
-     const {userId} = await auth()
+      
+     try {
 
-     if(!userId){
+        const {userId} = await auth()
+
+       if(!userId){
         return NextResponse.json({error : "unauthorized"},{status:401})
-     }
+       }
 
-     if(!process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME ||
+       if(!process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME ||
         !process.env.CLOUDINARY_API_KEY ||
         !process.env.CLOUDINARY_API_SECRET
-     ){
+       ){
         return NextResponse.json({error : "Cloudinary credentials not found"},{status : 500})
-     }
-     
-     try {
+       }  
         
         const formData = await request.formData()
         const file = formData.get("file") as File | null
-        
+        const title = formData.get("title") as string
+        const description = formData.get("descripton") as string
+        const originalSize = formData.get("originalSize") as string
+
         if(!file){
           return NextResponse.json({error : "File not found"},{status : 400})  
         } 
