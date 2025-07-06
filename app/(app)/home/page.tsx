0814195568c530e1aea518b33453e7b13e1e3b72
Anirban Kapil <anirbankapil@gmail.com>
2025-07-06 22:37:36 +0530

    add video fetching functionality and Video type definition

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+"use client"
+import axios from "axios"
+import VideoCard from "@/components/VideoCard"
+import { useCallback, useState } from "react"
+import { Video } from "@/types"
+
+
+
 
 function Home() {
+  
+  const [video,setVideo] = useState<Video[]>([])
+  const [loading,setLoading] = useState(false)
+  const [error,setError] = useState(false)
+
+  const fetchVideos = useCallback(async ()=>{
+    try {
+      const response = await axios.get("/api/videos")
+      if(Array.isArray(response.data)){
+        setVideo(response.data)
+      }
+    } catch (error) {
+      console.log(error);
+            setError("Failed to fetch videos")
+    } finally {
+            setLoading(false)
+    }         
+  },[])
+
   return (
     <div>Home</div>
   )
